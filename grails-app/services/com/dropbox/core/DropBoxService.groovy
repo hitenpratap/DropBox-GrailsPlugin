ad01b0f5da888e05550fbdb969d22b0352c8a71e
Hiten Pratap Singh <hitenpratap99@gmail.com>
2014-02-02 22:09:54 +0530

    First commit includes all necessary grails file for plugin

---
+++ grails-app/services/com/dropbox/core/DropBoxService.groovy
@@ -0,0 +1,16 @@
+package com.dropbox.core
+/**
+ * Created by hitenpratap on 2/2/14.
+ */
+class DropBoxService {
+
+    def getTempCode(String clientId) {
+        //TODO:code for getting temp code from DropBox
+    }
+
+    def generateAccessToken(String tempCode) {
+        //TODO:code for generate access token
+    }
+
+
+}
