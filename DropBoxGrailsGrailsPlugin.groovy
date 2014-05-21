348e80590b23c4a7a9fc892ac47dd7a874ea42b7
Hiten Pratap Singh <hitenpratap99@gmail.com>
2014-05-22 03:32:49 +0530

    Added codes for file operations like create,move,delete and copy

--- DropBoxGrailsGrailsPlugin.groovy
+++ DropBoxGrailsGrailsPlugin.groovy
@@ -1,12 +1,12 @@
 class DropBoxGrailsPlugin {
-    def version = '0.1'
+    def version = '1.0.1'
     def grailsVersion = '2.0 > *'
     def title = 'Drop Box Plugin for Grails'
     def description = 'Adds integration with the DropBox API. Here with this plugin you can use all DropBox services within from your Grails application'
     def documentation = 'https://github.com/hitenpratap/DropBox-GrailsPlugin/wiki/DropBox-for-Grails'
     def license = 'APACHE'
     def developers = [
-        [name: 'Hiten Pratap Singh', email: 'hitenpratap99@gmail.com']
+        [name: 'Hiten Pratap Singh', email: 'hitenpratap99@gmail.com',github:'https://github.com/hitenpratap']
     ]
     def issueManagement = [system: 'GITHUB', url: 'https://github.com/hitenpratap/DropBox-GrailsPlugin/issues']
     def scm = [url: 'https://github.com/hitenpratap/DropBox-GrailsPlugin']
--- grails-app/services/com/dropbox/core/DropBoxService.groovy
+++ grails-app/services/com/dropbox/core/DropBoxService.groovy
@@ -57,4 +57,109 @@ class DropBoxService {
     }
 
 
+    def createNewFolder(String root,String path,String accessToken){
+        StringBuilder tokenUri=new StringBuilder("root=")
+        tokenUri.append(URLEncoder.encode(root,"UTF-8"))
+        tokenUri.append("&path=")
+        tokenUri.append(URLEncoder.encode(path,"UTF-8"))
+        URL url=new URL("https://api.dropbox.com/1/fileops/create_folder?access_token=${accessToken}")
+        HttpURLConnection connection
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", "" + tokenUri.toString().length());
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
+            outputStreamWriter.write(tokenUri.toString());
+            outputStreamWriter.flush();
+            InputStreamReader inputStreamReader=new InputStreamReader(connection.getInputStream())
+            String response=inputStreamReader.text
+            return response
+
+        } finally {
+            connection?.disconnect()
+        }
+    }
+
+    def copyOps(String root,String fromPath,String toPath,String accessToken) {
+        StringBuilder tokenUri=new StringBuilder("root=")
+        tokenUri.append(URLEncoder.encode(root,"UTF-8"))
+        tokenUri.append("&from_path=")
+        tokenUri.append(URLEncoder.encode(fromPath,"UTF-8"))
+        tokenUri.append("&to_path=")
+        tokenUri.append(URLEncoder.encode(toPath,"UTF-8"))
+        URL url=new URL("https://api.dropbox.com/1/fileops/copy?access_token=${accessToken}")
+        HttpURLConnection connection
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", "" + tokenUri.toString().length());
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
+            outputStreamWriter.write(tokenUri.toString());
+            outputStreamWriter.flush();
+            InputStreamReader inputStreamReader=new InputStreamReader(connection.getInputStream())
+            String response=inputStreamReader.text
+            return response
+
+        } finally {
+            connection?.disconnect()
+        }
+    }
+
+    def deleteFileOps(String root,String path,String accessToken){
+        StringBuilder tokenUri=new StringBuilder("root=")
+        tokenUri.append(URLEncoder.encode(root,"UTF-8"))
+        tokenUri.append("&path=")
+        tokenUri.append(URLEncoder.encode(path,"UTF-8"))
+        URL url=new URL("https://api.dropbox.com/1/fileops/delete?access_token=${accessToken}")
+        HttpURLConnection connection
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", "" + tokenUri.toString().length());
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
+            outputStreamWriter.write(tokenUri.toString());
+            outputStreamWriter.flush();
+            InputStreamReader inputStreamReader=new InputStreamReader(connection.getInputStream())
+            String response=inputStreamReader.text
+            return response
+
+        } finally {
+            connection?.disconnect()
+        }
+    }
+
+    def moveOps(String root,String fromPath,String toPath,String accessToken) {
+        StringBuilder tokenUri=new StringBuilder("root=")
+        tokenUri.append(URLEncoder.encode(root,"UTF-8"))
+        tokenUri.append("&from_path=")
+        tokenUri.append(URLEncoder.encode(fromPath,"UTF-8"))
+        tokenUri.append("&to_path=")
+        tokenUri.append(URLEncoder.encode(toPath,"UTF-8"))
+        URL url=new URL("https://api.dropbox.com/1/fileops/move?access_token=${accessToken}")
+        HttpURLConnection connection
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", "" + tokenUri.toString().length());
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
+            outputStreamWriter.write(tokenUri.toString());
+            outputStreamWriter.flush();
+            InputStreamReader inputStreamReader=new InputStreamReader(connection.getInputStream())
+            String response=inputStreamReader.text
+            return response
+
+        } finally {
+            connection?.disconnect()
+        }
+    }
+
+
 }
